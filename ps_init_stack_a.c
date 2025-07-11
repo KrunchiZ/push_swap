35447f0deab5aa4e3b40c6495e97121b0e6fcf9b
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-07-11 13:34:18 +0800

    finished parsing the int values into a table for simplifying. Learn quick sort next

--- ps_init_stack_a.c
+++ ps_init_stack_a.c
@@ -6,13 +6,14 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 17:26:25 by kchiang           #+#    #+#             */
-/*   Updated: 2025/07/10 20:09:45 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/07/11 13:33:15 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 static void	ps_simplify_a(t_vars *set);
+static void	ps_sort_int_tab(int *array, size_t size);
 
 void	ps_init_stack_a(t_vars *set)
 {
@@ -39,13 +40,53 @@ void	ps_init_stack_a(t_vars *set)
 	}
 	set->a = a;
 	ps_simplify_a(set);
-	return (ps_free_args(set->args));
+	ps_free_args(set->args);
 }
 
 static void	ps_simplify_a(t_vars *set)
 {
-	//simplify the numbers to basic index numbers.
-	//how-to:
-	// -use a int array of args_size, parse all the number in.
-	// -sort the array then assign the index to their respective nb in stack a.
+	int		*tab;
+	size_t	i;
+	t_stack	*tmp;
+
+	tab = malloc(sizeof(int) * set->args_size);
+	if (!tab)
+	{
+		ps_free_args(set->args);
+		ps_lstclear(&a);
+		ps_error_abort();
+	}
+	tmp = set->a;
+	i = 0;
+	while (i < set->args_size)
+	{
+		tab[i] = tmp->nb;
+		i++;
+		tmp = tmp->next;
+	}
+	ps_sort_int_tab(tab, set->args_size);
+}
+
+static void	ps_sort_int_tab(int *tab, size_t size)
+{
+	int		i;
+	int		temp;
+	size_t	round;
+
+	round = 0;
+	while (round < size)
+	{
+		i = 0;
+		while (i < (size - 1))
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+			}
+			i++;
+		}
+		round++;
+	}
 }
