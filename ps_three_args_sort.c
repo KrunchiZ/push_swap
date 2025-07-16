e375eacde605d2e655cb19800f12483f8d254d14
Kai Zhi Chiang <kchiang@u91z05s06.42kl.edu.my>
2025-07-16 14:28:11 +0800

    added missing static functions prototype in ps_three_args_sort.c

--- ps_three_args_sort.c
+++ ps_three_args_sort.c
@@ -1,17 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ps_sort_three.c                                    :+:      :+:    :+:   */
+/*   ps_three_args_sort.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 13:17:16 by kchiang           #+#    #+#             */
-/*   Updated: 2025/07/16 13:20:23 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/07/16 14:27:32 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static int	ps_min_at_top(t_stack *a);
+static int	ps_min_in_middle(t_stack *a);
+
+/* Sorting algorithm for three arguments stack.
+ * */
 void	ps_three_args_sort(t_vars *set)
 {
 	t_stack	*last;
