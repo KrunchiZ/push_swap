d8df7c97a54287dd8bbcf5ac532c67f1f7c94f51
Kai Zhi Chiang <kchiang@u91z05s06.42kl.edu.my>
2025-07-16 15:34:25 +0800

    fixed the logic in three_args_sort from min check to max check

--- ps_three_args_sort.c
+++ ps_three_args_sort.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 13:17:16 by kchiang           #+#    #+#             */
-/*   Updated: 2025/07/16 15:29:29 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/07/16 15:34:03 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	ps_three_args_sort(t_vars *set)
 	t_stack	*a;
 
 	a = set->a;
-	if (a->next->index < a->index && a->next->index < a->previous->index)
+	if (a->next->index > a->index && a->next->index > a->previous->index)
 		ps_exec_rotate(set, RRA);
-	else if (a->index < a->previous->index && a->index < a->next->index)
+	else if (a->index > a->previous->index && a->index > a->next->index)
 		ps_exec_rotate(set, RA);
 	if (a->index > a->next->index)
 		ps_exec_swap(set, SA);
